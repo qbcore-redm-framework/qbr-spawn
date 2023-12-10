324adb41bfa80441edf8640c13265ce2f7c87c4a
Hashisx <9405309+hashisx@users.noreply.github.com>
2023-12-10 09:47:48 -0800

    fix spawn for existing players

--- client.lua
+++ client.lua
@@ -172,9 +172,12 @@ RegisterNUICallback('spawnplayer', function(data)
         Wait(500)
         DoScreenFadeIn(250)
     elseif type == "normal" then
-        local pos = QB.Spawns[location].coords
+        local pos = {}
+
         if newPlayer then
             pos = QB.FirstSpawns[location].coords
+        else
+            pos = QB.Spawns[location].coords
         end
         SetDisplay(false)
         DoScreenFadeOut(500)
